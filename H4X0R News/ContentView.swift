c1a9c81a9292a31add82d296bcc2ff56511ebcab
yaronmd <yaronmord@gmail.com>
2020-01-19 09:47:52 +0200

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by yaron mordechai on 19/01/2020.
+//  Copyright © 2020 yaron mordechai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
